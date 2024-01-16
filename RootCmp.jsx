75c284bfdac6897a363dfd3867c1d4a8e645318b
shrfrn <shrfrn@gmail.com>
2024-01-16 09:29:58 +0200

    Init repo

---
+++ RootCmp.jsx
@@ -0,0 +1,11 @@
+export function RootCmp() {
+    return <section className="app">
+        <header>
+            <h1>My App</h1>
+        </header>
+
+        <main>
+            { /* TODO: Create a component */ }
+        </main>
+    </section>
+}
